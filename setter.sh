187ff3ccd7852fa5caa3f998ce6c001aa235193d
Rees Pozzi <47995122+reespozzi@users.noreply.github.com>
2022-03-18 21:48:52 +0000

    Update setter.sh

--- setter.sh
+++ setter.sh
@@ -3,12 +3,12 @@ set -e
 
 cd 
 if [[ -f ".zshrc" ]];then 
-  read -q "deleteFile?-----  This will delete your current ~/.zshrc file. Continue? [y/n]  ----- "
+  read -q "deleteFile?-----  This will delete your current ~/.zshrc file. Continue? (n to append to current file)[y/n]  ----- "
   if [[ ${deleteFile} == "y" ]];then
     printf "\nExecuting...\n"
     rm .zshrc
   else
-    printf "\nCancelled $1."
+    printf "\nCancelled, appending instead."
   fi
 fi
 cat ./Prod-K8S-Command-Sanity-Check/file >> ~/.zshrc
